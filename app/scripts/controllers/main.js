999cbe03569d707252bf0e114400c3d80585eccc
wus <swunderlin@gmail.com>
2016-02-17 14:10:44 +0100

    added

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularDevApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularDevApp
+ */
+angular.module('angularDevApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
